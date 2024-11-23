d5b94e357c70fd385d7d7ca02ec53e416e523ace
johe22qq <johe22qq@student.ju.se>
2024-11-23 03:57:27 +0100

    GÖR OM LOBBYSCREEN

---
+++ app/src/main/java/com/example/firebase/GameModel.kt
@@ -0,0 +1,43 @@
+package com.example.firebase
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class GameModel: ViewModel() {
+    val db = Firebase.firestore
+    var localPlayerId = mutableStateOf<String?>(null)
+    val playerMap = MutableStateFlow<Map<String, Player>>(emptyMap())
+    val gameMap = MutableStateFlow<Map<String, Game>>(emptyMap())
+
+    fun initGame() {
+
+        db.collection("players")
+            .addSnapshotListener { value, error ->
+                if (error != null) {
+                    return@addSnapshotListener
+                }
+                if (value != null) {
+                    val updatedMap = value.documents.associate { doc ->
+                        doc.id to doc.toObject(Player::class.java)!!
+                    }
+                    playerMap.value = updatedMap
+                }
+            }
+
+        db.collection("games")
+            .addSnapshotListener { value, error ->
+                if (error != null) {
+                    return@addSnapshotListener
+                }
+                if (value != null) {
+                    val updatedMap = value.documents.associate { doc ->
+                        doc.id to doc.toObject(Game::class.java)!!
+                    }
+                    gameMap.value = updatedMap
+                }
+            }
+    }
+}
--- app/src/main/java/com/example/firebase/LobbyScreen.kt
+++ app/src/main/java/com/example/firebase/LobbyScreen.kt
@@ -2,22 +2,31 @@ package com.example.firebase
 
 import android.content.Context
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -30,49 +39,61 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LobbyScreen(navController: NavHostController) { // inkuderat Nav, måste ha en gamemodell
-    val db = Firebase.firestore
-    val coroutineScope = rememberCoroutineScope()
 
-    val playerList = remember { MutableStateFlow<List<Player>>(emptyList()) }
-    val gameMap = remember { MutableStateFlow<Map<String, Game>>(emptyMap()) }
 
 // vi behöver hämta spelarens id i nån currentvariabel och matcha databasens id,  inspiererad av lab5 ex
-    val sharedPreferences = LocalContext.current.getSharedPreferences("TicTacToePrefs", Context.MODE_PRIVATE)
-        .getString("playerId","")
+    val sharedPreferences =
+        LocalContext.current.getSharedPreferences("TicTacToePrefs", Context.MODE_PRIVATE)
+            .getString("playerId", "")
 
-// behöver ändra dernna en dekl
-    LaunchedEffect(Unit) {
-        db.collection("players").addSnapshotListener { value, error ->
-            if (error == null && value != null) {
-                val players = value.toObjects(Player::class.java)
-                coroutineScope.launch {
-                    playerList.emit(value.toObjects(Player::class.java))
+    var opponentName by remember { mutableStateOf("") }
+    var errorMessage by remember { mutableStateOf("") }
+
+
+//BEHÖVER SKAPA DÄR MAN KAN ANGE MOTSTÅNDARENAMN OCH ATT MAN KAN UTMANA!
+
+    Scaffold(
+        content = { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+                    .padding(25.dp)
+            ) {
+                Text("ENTER OPONANTS NAME")
+                OutlinedTextField(
+                    value = opponentName,
+                    onValueChange = { opponentName = it },
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(
+                    onClick = {
+                        if (opponentName.isNotBlank()) {
+                            // fixa det praktiska med att skicka en chalange
+
+                        }
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text("SEND CHALLENGE")
                 }
             }
         }
-    }
-
-    db.collection("games")
-        .addSnapshotListener { value, error ->
-            if (error != null) {
-                return@addSnapshotListener
-            }
-            if (value != null) {
-                val updatedMap = value.documents.associate { doc ->
-                    doc.id to doc.toObject(Game::class.java)!!
-                }
-                gameMap.value = updatedMap
-            }
-        }
+    )
+}
 
 
-    //nedan för behöver jag fortsätta att ändra
 
-    val players by playerList.collectAsStateWithLifecycle()
-    val games by gameList.collectAsStateWithLifecycle()
 
+
+    /*
+
+    behöver göra om denna helt,
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
         LazyColumn(modifier = Modifier.padding(innerPadding)) {
@@ -136,3 +157,5 @@ fun LobbyScreen(navController: NavHostController) { // inkuderat Nav, måste ha 
         }
     }
 }
+/*
+     */
