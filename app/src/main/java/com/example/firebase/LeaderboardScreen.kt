c70d8cb31dabe091363516e794307394b53e5c1b
johe22qq <johe22qq@student.ju.se>
2024-11-28 10:18:13 +0100

    forts

--- app/src/main/java/com/example/firebase/LeaderboardScreen.kt
+++ app/src/main/java/com/example/firebase/LeaderboardScreen.kt
@@ -1,12 +1,19 @@
 package com.example.firebase
 
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import com.google.firebase.firestore.ktx.firestore
@@ -38,6 +45,28 @@ fun LeaderboardScreen(model: GameModel) {
             players = listOFplayers
 
         }
+
+    }
+    LazyColumn (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ){
+        items(players) { player ->
+            Text(
+                text = "${player.name}: ${player.score}",
+                modifier = Modifier
+                    .padding(10.dp)
+
+
+            )
+
+    }
+
     }
 
 }
+
+
+
+
