211e79711dcd065f5fd69d2d26905a0c0e3fd759
johe22qq <johe22qq@student.ju.se>
2024-11-29 10:45:50 +0100

    Column leaderboard

--- app/src/main/java/com/example/firebase/LeaderboardScreen.kt
+++ app/src/main/java/com/example/firebase/LeaderboardScreen.kt
@@ -1,6 +1,8 @@
 package com.example.firebase
 
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -13,6 +15,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -49,38 +52,46 @@ fun LeaderboardScreen(model: GameModel, navController: NavController) {
         }
 
     }
-    LazyColumn (
+
+    Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
-    ){
-        items(players) { player ->
-            Text(
-                text = "${player.name}: ${player.score}",
-                modifier = Modifier
-                    .padding(10.dp)
+            .padding(20.dp),
 
-
-            )
-    }
-}
-    Button(
-        onClick = {
-            navController.navigate("LobbyScreen")
-        },
-
-        modifier = Modifier
-            .padding(top=300.dp)
-            .padding(start = 70.dp)
-
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
 
 
     ) {
-        Text("Gå till lobbyn och spela igen! ")
 
 
+        LazyColumn(
+            modifier = Modifier
+               // .fillMaxSize()
+                .padding(16.dp)
+
+        ) {
+            items(players) { player ->
+                Text(
+                    text = "${player.name}: ${player.score}",
+                    modifier = Modifier
+                        .padding(10.dp)
+
+
+                )
+            }
+        }
+        Button(
+            onClick = {
+                navController.navigate("LobbyScreen")
+            },
+
+        ) {
+            Text("Gå till lobbyn och spela igen! ")
+
+
+        }
     }
-
 
 }
 
